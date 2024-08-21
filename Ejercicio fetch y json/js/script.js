9236b24ce7df962a109aa1b6871f9770f7d48711
sofiacarrion1 <sofiacarrion2815@gmail.com>
2024-08-21 20:04:17 -0300

    main

--- Ejercicio fetch y json/js/script.js
+++ Ejercicio fetch y json/js/script.js
@@ -11,11 +11,40 @@ const container = document.getElementById("container"); // "Traemos" utilizando 
  * imprime el campo "name" y el campo "lastname" separados por un espacio
  */
 function showData(dataArray) {
+  
+  container.innerHTML = '';
+
   // El for itera sobre los elementos del array
-  for (const item of dataArray) {
-    // En la siguiente línea se utilizan "backticks" para armar el String. Más info => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Template_literals
-    container.innerHTML += `<p> ${item.name} ${item.lastname} </p>`; // Se concatena cada párrafo de la manera que queremos mostrarlo al innerHTML del contenedor
+    for (const item of dataArray) {
+      container.innerHTML += `
+        <div class="list-item">
+          <h5>${item.name} ${item.lastname}</h5>
+          <p>Edad: ${item.age}</p>
+        </div>`;
+    }
   }
-}
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+fetch(DATA_URL)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); 
+  })
+  .then(data => {
+    const students = data.students;
+    showData(students); 
+  })
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+  });
+
+document.addEventListener("DOMContentLoaded", function(){
+  getJSONData(DATA_URL).then(function(response){
+      if(response.status === 'ok'){
+          console.log(response.data.Company.Employees);
+          showEmployees(response.data.Company.Employees)
+      }
+  })
+});
